349b85592d8a3f53d367e9bbd6c063f291eeabf0
tgermakovski <78508052+tgermakovski@users.noreply.github.com>
2024-03-27 09:17:15 -0400

    Update Main.java
    added all the class inside the main file

--- Main.java
+++ Main.java
@@ -124,3 +124,113 @@ public class Main {
     }
 }
 
+public class Time {
+
+    double r; int c;
+
+    Time(double rr, int cc){r=rr;c=cc;}
+
+    public Time timeAdvance(Time interval, int p){
+
+        if(p>0){
+            if(interval.r == 0){
+                return new Time(this.r, this.c + interval.c);
+            }else{return new Time(this.r + interval.r, 0);}
+        }else return new Time(Double.POSITIVE_INFINITY,0);
+
+    }
+
+
+}
+
+public class Press {
+
+    int p = 0; //number of parts in the system
+    double s = Double.POSITIVE_INFINITY; //time remaining to press part now being pressed
+    int t=1; //time it takes to press one part
+
+    Press(){}
+
+    int lambda(){return 1;}
+
+    void deltaInternal(){p--; s=t;}
+
+    void deltaExternal(double e, int q){
+        if(p>0){
+            s-=e;
+        }else{s=t;}
+        p+=q;
+    }
+
+    void deltaConfluent(double e, int q){p += q-1; s=t;}
+
+}
+
+public class Event implements Comparable<Event>{
+
+    Time t;
+    int x; int xx;
+    boolean pressExt;
+    boolean pressInt;
+    boolean pressCon;
+    boolean drillExt;
+    boolean drillInt;
+    boolean drillCon;
+
+
+    Event(Time t, int x, int xx, boolean pe, boolean pi, boolean pc, boolean de, boolean di, boolean dc){
+        this.t=t; this.x=x; this.xx=xx;
+        pressExt=pe;
+        pressInt=pi;
+        pressCon=pc;
+        drillExt=de;
+        drillInt=di;
+        drillCon=dc;
+    }
+
+
+    @Override
+    public int compareTo(Event o) {
+        // Compare based on r values
+        if (t.r > o.t.r) {
+            return 1;
+        } else if (t.r < o.t.r) {
+            return -1;
+        } else {
+            // If r values are equal, compare based on c values
+            if (t.c > o.t.c) {
+                return 1;
+            } else if (t.c < o.t.c) {
+                return -1;
+            } else {
+                // If both r and c values are equal, return 0 (objects are equal)
+                return 0;
+            }
+        }
+    }
+
+}
+
+public class Drill {
+
+    int p = 0; //number of parts in the system
+    double s = Double.POSITIVE_INFINITY; //time remaining to drill part now being drilled
+    int t=2; //time it takes to drill one part
+
+    Drill(){}
+
+    int lambda(){System.out.println("1");return 1;}
+
+    void deltaInternal(){p--; s=t;}
+
+    void deltaExternal(double e, int q){
+        if(p>0){
+            s-=e;
+        }else{s=t;}
+        p+=q;
+    }
+
+    void deltaConfluent(double e, int q){p += q-1; s=t;}
+
+}
+
